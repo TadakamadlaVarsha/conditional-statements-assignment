217782a007947ca74553100f2dcffe200b4385ca
varsha <tadakamadlavarsha@gmail.com>
2025-08-16 13:46:25 +0530

    added solution for question2

---
+++ deliveryapp/app.js
@@ -0,0 +1,18 @@
+let orderamount=450;
+let ispremiumMember=false;
+let isweekday=true;
+let ordertime=19;
+let usedcoupon=false;
+let issunday=true;
+let ismorning=true;
+let benefit="";
+if(orderamount>1000 && ispremiumMember){
+    benefit="free delivery + 15% off";
+}
+else if(orderamount>=500 && orderamount<=1000 && isweekday && ordertime >=18 && ordertime>=20 && !usedcoupon){
+    benefit="$100 discount";
+}
+else if(orderamount<500 && issunday && ismorning){
+    benefit="free delivery";
+}
+console.log(`benefit:${benefit}`);
