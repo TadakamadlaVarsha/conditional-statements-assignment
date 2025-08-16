6772e379077fcbb757a555efa39cff28b5c9e48f
varsha <tadakamadlavarsha@gmail.com>
2025-08-16 13:58:27 +0530

    added solution for question2

--- deliveryapp/app.js
+++ deliveryapp/app.js
@@ -9,10 +9,13 @@ let benefit="";
 if(orderamount>1000 && ispremiumMember){
     benefit="free delivery + 15% off";
 }
-else if(orderamount>=500 && orderamount<=1000 && isweekday && ordertime >=18 && ordertime>=20 && !usedcoupon){
+else if(orderamount>=500 && orderamount<=1000 && isweekday && ordertime >=18 && ordertime<=20 && !usedcoupon){
     benefit="$100 discount";
 }
 else if(orderamount<500 && issunday && ismorning){
     benefit="free delivery";
+}
+else{
+    console.log("they have to pay full amount")
 }
 console.log(`benefit:${benefit}`);
